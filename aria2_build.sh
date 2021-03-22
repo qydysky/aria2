35f192155a845a7d4ae321775d9491e1be824ffd
qydysky <qydysky@foxmail.com>
2021-03-22 14:55:39 +0800

    init

--- aria2_build.sh
+++ aria2_build.sh
@@ -9,6 +9,65 @@ else
 CRT='./certs/ca-certificates.crt'
 fi
 
+#c-ares
+rm -rf c-ares_git
+git clone --depth=1 https://github.com/c-ares/c-ares.git c-ares_git
+cd c-ares_git
+autoreconf -fi
+./configure \
+    --disable-shared \
+    --enable-static \
+    --without-random \
+    --disable-tests 
+
+make install -j$CPUCOUNT
+cd ../
+rm -rf c-ares_git
+#c-ares
+
+#expat
+rm -rf libexpat_git
+git clone --depth=1 https://github.com/libexpat/libexpat.git libexpat_git
+cd libexpat_git/expat
+./buildconf.sh
+./configure \
+    --disable-shared \
+    --enable-static 
+    
+make install -j$CPUCOUNT
+cd ../../
+rm -rf libexpat_git 
+#expat
+
+#libssh2
+rm -rf libssh2_git
+git clone --depth=1 https://github.com/libssh2/libssh2.git libssh2_git
+cd libssh2_git
+autoreconf -fi
+./configure \
+    --disable-shared \
+    --enable-static \
+    --with-crypto=wincng \
+    LIBS="-lws2_32"
+
+make install -j$CPUCOUNT
+cd ../
+rm -rf libssh2_git
+#libssh2
+
+#sql
+rm -rf sqlite_git
+git clone --depth=1 https://github.com/sqlite/sqlite.git sqlite_git
+cd sqlite_git
+./configure \
+    --disable-shared \
+    --enable-static 
+
+make install -j$CPUCOUNT
+cd ../
+rm -rf sqlite_git 
+#sql
+
 rm -rf aria2_git
 git clone --depth=1 https://github.com/aria2/aria2.git aria2_git
 cd aria2_git
@@ -30,7 +89,7 @@ autoreconf -fi
     ARIA2_STATIC=yes \
     --enable-shared=no
 
-make
+make -j$CPUCOUNT
 cd ..
-cp aria2_git/src/aria2c ./aria2c
+cp aria2_git/src/aria2c* ./
 rm -rf aria2_git
--- aria2_win_build.sh
+++ aria2_win_build.sh
@@ -10,95 +10,17 @@ MINGW64)
     ;;
 esac
 
-# workaround git user name and email not set
-GIT_USER_NAME="$(git config --global user.name)"
-GIT_USER_EMAIL="$(git config --global user.email)"
-if [[ "${GIT_USER_NAME}" = "" ]]; then
-    git config --global user.name "Name"
-fi
-if [[ "${GIT_USER_EMAIL}" = "" ]]; then
-    git config --global user.email "you@example.com"
-fi
-
 pacman -S --noconfirm --needed $MINGW_PACKAGE_PREFIX-gcc \
     $MINGW_PACKAGE_PREFIX-winpthreads
 
 PREFIX=/usr/local/$HOST
 CPUCOUNT=$(grep -c ^processor /proc/cpuinfo)
-curl_opts=(/usr/bin/curl --connect-timeout 15 --retry 3
-    --retry-delay 5 --silent --location --insecure --fail)
 
-clean_html_index() {
-    local url="$1"
-    local filter="${2:-(?<=href=\")[^\"]+\.(tar\.(gz|bz2|xz)|7z)}"
-    "${curl_opts[@]}" -l "$url" | grep -ioP "$filter" | sort -uV
-}
-
-clean_html_index_sqlite() {
-    local url="$1"
-    local filter="${2:-(\d+\/sqlite-autoconf-\d+\.tar\.gz)}"
-    "${curl_opts[@]}" -l "$url" | grep -ioP "$filter" | sort -uV | tail -1
-}
-
-get_last_version() {
-    local filelist="$1"
-    local filter="$2"
-    local version="$3"
-    local ret
-    ret="$(echo "$filelist" | /usr/bin/grep -E "$filter" | sort -V | tail -1)"
-    [[ -n "$version" ]] && ret="$(echo "$ret" | /usr/bin/grep -oP "$version")"
-    echo "$ret"
-}
-
-# expat
-expat_ver="$(clean_html_index https://sourceforge.net/projects/expat/files/expat/ 'expat/[0-9]+\.[0-9]+\.[0-9]+')"
-expat_ver="$(get_last_version "${expat_ver}" expat '2\.\d+\.\d+')"
-expat_ver="${expat_ver:-2.2.10}"
-wget -c --no-check-certificate "https://downloads.sourceforge.net/project/expat/expat/${expat_ver}/expat-${expat_ver}.tar.bz2"
-tar xf "expat-${expat_ver}.tar.bz2"
-cd "expat-${expat_ver}" || exit 1
-./configure \
-    --disable-shared \
-    --enable-static \
-    --prefix=/usr/local/$HOST \
-    --host=$HOST
-make install -j$CPUCOUNT
-cd ..
-rm -rf "expat-${expat_ver}"
-
-# sqlite
-sqlite_ver=$(clean_html_index_sqlite "https://www.sqlite.org/download.html")
-[[ ! "$sqlite_ver" ]] && sqlite_ver="2020/sqlite-autoconf-3340000.tar.gz"
-sqlite_file=$(echo ${sqlite_ver} | grep -ioP "(sqlite-autoconf-\d+\.tar\.gz)")
-wget -c --no-check-certificate "https://www.sqlite.org/${sqlite_ver}"
-tar xf "${sqlite_file}"
-echo ${sqlite_ver}
-sqlite_name=$(echo ${sqlite_ver} | grep -ioP "(sqlite-autoconf-\d+)")
-cd "${sqlite_name}" || exit 1
-./configure \
-    --disable-shared \
-    --enable-static \
-    --prefix=/usr/local/$HOST \
-    --host=$HOST
-make install -j$CPUCOUNT
-cd ..
-rm -rf "${sqlite_name}"
-
-# c-ares: Async DNS support
-[[ ! "$cares_ver" ]] &&
-    cares_ver="$(clean_html_index https://c-ares.haxx.se/)" &&
-    cares_ver="$(get_last_version "$cares_ver" c-ares "1\.\d+\.\d")"
-cares_ver="${cares_ver:-1.17.1}"
-echo "c-ares-${cares_ver}"
-wget -c --no-check-certificate "https://c-ares.haxx.se/download/c-ares-${cares_ver}.tar.gz"
-tar xf "c-ares-${cares_ver}.tar.gz"
-cd "c-ares-${cares_ver}" || exit 1
-# https://github.com/c-ares/c-ares/issues/384
-# https://github.com/c-ares/c-ares/commit/c35f8ff50710cd38776e9560389504dbd96307fa
-if [ "${cares_ver}" = "1.17.1" ]; then
-    patch -p1 < ../c-ares-1.17.1-fix-autotools-static-library.patch
-    autoreconf -fi || autoreconf -fiv
-fi
+#c-ares
+rm -rf c-ares_git
+git clone --depth=1 https://github.com/c-ares/c-ares.git c-ares_git
+cd c-ares_git
+autoreconf -fi
 ./configure \
     --disable-shared \
     --enable-static \
@@ -106,24 +28,33 @@ fi
     --disable-tests \
     --prefix=/usr/local/$HOST \
     --host=$HOST
-make install -j$CPUCOUNT
-cd ..
-rm -rf "c-ares-${cares_ver}"
 
-# libssh2
-[[ ! "$ssh_ver" ]] &&
-    ssh_ver="$(clean_html_index https://libssh2.org/download/)" &&
-    ssh_ver="$(get_last_version "$ssh_ver" tar.gz "1\.\d+\.\d")"
-ssh_ver="${ssh_ver:-1.9.0}"
-echo "${ssh_ver}"
-wget -c --no-check-certificate "https://libssh2.org/download/libssh2-${ssh_ver}.tar.gz"
-tar xf "libssh2-${ssh_ver}.tar.gz"
-cd "libssh2-${ssh_ver}"
-# https://github.com/libssh2/libssh2/pull/479
-# https://github.com/libssh2/libssh2/commit/ba149e804ef653cc05ed9803dfc94519ce9328f7
-if [ "${ssh_ver}" = "1.9.0" ]; then
-    patch -p1 < ../libssh2-1.9.0-wincng-multiple-definition.patch
-fi
+make install -j$CPUCOUNT
+cd ../
+rm -rf c-ares_git
+#c-ares
+
+#expat
+rm -rf libexpat_git
+git clone --depth=1 https://github.com/libexpat/libexpat.git libexpat_git
+cd libexpat_git/expat
+./buildconf.sh
+./configure \
+    --disable-shared \
+    --enable-static \
+    --prefix=/usr/local/$HOST \
+    --host=$HOST
+    
+make install -j$CPUCOUNT
+cd ../../
+rm -rf libexpat_git 
+#expat
+
+#libssh2
+rm -rf libssh2_git
+git clone --depth=1 https://github.com/libssh2/libssh2.git libssh2_git
+cd libssh2_git
+autoreconf -fi
 ./configure \
     --disable-shared \
     --enable-static \
@@ -131,9 +62,26 @@ fi
     --host=$HOST \
     --with-crypto=wincng \
     LIBS="-lws2_32"
+
 make install -j$CPUCOUNT
-cd ..
-rm -rf "libssh2-${ssh_ver}"
+cd ../
+rm -rf libssh2_git
+#libssh2
+
+#sql
+rm -rf sqlite_git
+git clone --depth=1 https://github.com/sqlite/sqlite.git sqlite_git
+cd sqlite_git
+./configure \
+    --disable-shared \
+    --enable-static \
+    --prefix=/usr/local/$HOST \
+    --host=$HOST
+
+make install -j$CPUCOUNT
+cd ../
+rm -rf sqlite_git 
+#sql
 
 rm -rf aria2_git
 git clone --depth=1 https://github.com/aria2/aria2.git aria2_git
@@ -163,3 +111,6 @@ autoreconf -fi || autoreconf -fiv
     LDFLAGS="-L$PREFIX/lib -Wl,--gc-sections,--build-id=none" \
     PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 make -j$CPUCOUNT
+cd ..
+cp aria2_git/src/aria2c* ./
+rm -rf aria2_git
